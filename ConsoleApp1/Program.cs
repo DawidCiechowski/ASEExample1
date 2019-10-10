1aa649bf6261cfe276827f6d9134ae27986cc073
Ciechowski <D.Ciechowski2474@student.leedsbeckett.ac.uk>
2019-10-10 09:51:29 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+            Console.WriteLine(app.example1());
+        }
+
+
+
+
+        public string example1()
+        {
+            return "This is an example";
+        }
+    }
+}
+
